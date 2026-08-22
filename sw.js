7ca10b923ec3c63bc838f99c6ca289d1745644d0
zhizhi-ye <moonnazhi@gmail.com>
2026-08-23 04:08:27 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'lit-tracker-v1';
-const ASSETS = ['./', 'index.html', 'app.js', 'manifest.json', 'icons/icon-192.svg', 'icons/icon-512.svg'];
+const ASSETS = ['./', 'index.html', 'app.js', 'manifest.json', 'icons/icon-192.png', 'icons/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
